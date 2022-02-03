561c73a4bb648d9bbf14d4dcf1e395367ae7e4f9
Sumita2609 <72155593+Sumita2609@users.noreply.github.com>
2022-02-03 12:27:34 +0530

    Add files via upload

---
+++ ConnectionProvider.java
@@ -0,0 +1,30 @@
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Project;
+import java.sql.*;
+
+
+
+/**
+ *
+ * @author SUMITA
+ */
+public class ConnectionProvider {
+    public static Connection getCon()
+    {
+        try
+        {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/project?zeroDateTimeBehavior=CONVERT_TO_NULL","root","");
+            return con;
+        }
+    
+        catch(Exception e)
+        {
+            return null;
+        }
+    }    
+}
